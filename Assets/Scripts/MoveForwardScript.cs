cf3d976fa1bd5974ee0fddf6f2b14d4ac39d361a
artem88 <artemik2010@gmail.com>
2018-04-14 19:12:20 +0300

    Created new Scene with an environment Added GVR SDK Added script for Movement
    Target Platform: Android
    XR Settings: Cardboard SDK

---
+++ Assets/Scripts/MoveForwardScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForwardScript : MonoBehaviour
+{
+
+    public float speed = 3.0f;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") || Input.touchCount > 0)
+        {
+            Vector3 foward = Camera.main.transform.forward;
+            foward.y = 0;
+            this.transform.position += foward * Time.deltaTime * speed;
+        }
+    }
+}
